37e46089964cab428607488396b3e440da865e13
tattubak <a2103ti@aiit.co.jp>
2021-07-30 13:10:56 +0000

    present files

--- same-test.sh
+++ same-test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+
+
 echo "same" > /tmp/$$-ans
 ./same.sh a a > /tmp/$$-result
 diff /tmp/$$-ans /tmp/$$-result || exit 1
 
-echo "not same" > /tmp/$$-ans
-./same.sh a b > /tmp$$-result
-diff /tmp/$$-ans /tmp/$$-result || exit 1
 
+echo "not same" > /tmp/$$-ans
+./same.sh a b > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || exit 1
 rm /tmp/$$-*
+
+echo "vaild"
